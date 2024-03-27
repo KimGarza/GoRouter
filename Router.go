92fe5e901ab644a80709ce16821e52d1737e1b3b
Kim Corson <kimmc67b@gmail.com>
2024-03-26 21:51:30 -0500

    router object

---
+++ Router.go
@@ -0,0 +1,18 @@
+type Router struct {
+	NAT
+	PAT
+	Wireless
+	DHCP
+	Firewall
+	VPN
+	QoS
+	NetworkInterfacePorts
+	GuestAccess
+	Firmware
+	SecurityEncryption // (WPA, WPA2, WPA3)
+	IPv4
+	IPv6
+	ConfigurationInterface
+	Antennas
+	DMZ
+}
