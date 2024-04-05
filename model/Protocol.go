f34af7f56f50339b36a09760e509fa8c1fdd7067
Kim Corson <kimmc67b@gmail.com>
2024-04-05 08:55:06 -0500

    fully constructed http protocol

--- model/Protocol.go
+++ model/Protocol.go
@@ -1,10 +1,15 @@
 package model
 
 type Protocol struct {
-	Name string `json:"name"`
-	Port string `json:"port"`
-	// TransportLayer string
-	// Purpse         string
-	// Security       string
-	// Version        string
+	Name            string   `json:"name"`
+	Port            string   `json:"port"`
+	OSI             string   `json:"osi"`
+	ConnnectionType []string `json:"connectionType"`
+	Speed           int      `json:"speed"`
+	Security        string   `json:"security"`
+	Payload         []string `json:"payload"`
+	Headers         []string `json:"headers"`
+	Standards       []string `json:"standards"`
+	Description     string   `json:"description"`
+	LatestVersion   float64  `json:"latestVersion"`
 }
--- service/ProtocolBuilder.go
+++ service/ProtocolBuilder.go
@@ -9,11 +9,22 @@ func ProtocolBuilder(input string) (*model.Protocol, error) {
 
 	switch input {
 	case "80", "HTTP":
-		return &model.Protocol{Name: "HTTP", Port: "80"}, nil
-	case "22", "SSH":
-		return &model.Protocol{Name: "SSH", Port: "22"}, nil
-	case "3389", "RDP":
-		return &model.Protocol{Name: "RDP", Port: "3389"}, nil
+		return &model.Protocol{
+				Name:            "HTTP",
+				Port:            "80",
+				OSI:             "Application",
+				ConnnectionType: []string{"stateful", "stateless"},
+				Security:        "none",
+				Payload:         []string{"HTML", "CSS", "JavaScript", "Multimedia", "JSON", "XML", "Binary Data", "Form Data"}, // content type
+				Headers:         []string{"Content-Type", "Content-Length", "Request Headers", "Repsonse Headers", "Security Headers", "Entity Headers"},
+				Standards:       []string{"RFC 1945", "RFC 2616", "RFC 7230", "RFC 7231", "RFC 7232", "RFC 7233", "RFC 7234", "RFC 7235", "RFC 7540", "RFC 9114"},
+				Description:     "HTTP is an application layer protocol designed to transfer information between networked devices and runs on top of other layers of the network protocol stack. -Cloudflare",
+				LatestVersion:   3},
+			nil
+	// case "22", "SSH":
+	// 	return &model.Protocol{Name: "SSH", Port: "22"}, nil
+	// case "3389", "RDP":
+	// 	return &model.Protocol{Name: "RDP", Port: "3389"}, nil
 	default:
 		return nil, fmt.Errorf("Input did not match any protocols in directory")
 	}
